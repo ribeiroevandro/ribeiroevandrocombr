b36d13371367ffc93018b876b3e8d67253984562
Evandro Ribeiro <ribeiroevandro@live.com>
2024-03-06 15:47:25 -0300

    feat: define package manager and add next config file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone",
+  compress: false,
+};
+
+module.exports = nextConfig;
