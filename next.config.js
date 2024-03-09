aa5a2bbe6f59835d4dfebfaf261ec9f3b36c446f
Evandro Ribeiro <ribeiroevandro@live.com>
2024-03-09 02:08:44 -0300

    chore: add validation in next config file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false,
-  output: "export",
-  compress: false,
-  images: {
-    unoptimized: true
-  }
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: false,
+//   output: "export",
+//   compress: false,
+//   images: {
+//     unoptimized: true
+//   }
+// };
+//
+// module.exports = nextConfig;
+// @ts-check
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
-module.exports = nextConfig;
+module.exports = (phase) => {
+  let nextConfig;
+  if (phase && phase === PHASE_PRODUCTION_BUILD) {
+    return {}
+  }
+
+  return nextConfig = {
+    reactStrictMode: true,
+    swcMinify: false,
+    output: "export",
+    compress: false,
+    images: {
+      unoptimized: true
+    }
+  }
+}
